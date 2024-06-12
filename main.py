8de91fc8f4e191b8622b7b736fd40961c25a3c34
Lisin1 <150523149+Lisin1@users.noreply.github.com>
2024-06-12 15:31:39 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+password = input('Введите сложный пароль:')
+int_password = input('Введите пароль:')
+
+if int_password == password:
+    print('Приветсвуем вас!')
+else:
+    print('Неверный пароль >:(')
