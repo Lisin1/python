960d96416e7231dabc3e60903eb3860a7515b7f5
Lisin1 <150523149+Lisin1@users.noreply.github.com>
2024-12-22 20:26:45 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-password = input('Введите сложный пароль:')
-int_password = input('Введите пароль:')
-
-if int_password == password:
-    print('Приветсвуем вас!')
-else:
-    print('Неверный пароль >:(')
+print=("Hello")
